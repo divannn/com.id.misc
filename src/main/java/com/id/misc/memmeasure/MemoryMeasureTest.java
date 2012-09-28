47977bb244ac6915eeaf99714d7b91b9ee048206
divannn <unique-id@mail.ru>
2012-09-28 19:00:25 +0400

    Added cool test.

--- src/main/java/com/id/misc/memmeasure/MemoryMeasureTest.java
+++ src/main/java/com/id/misc/memmeasure/MemoryMeasureTest.java
@@ -11,13 +11,26 @@ import java.util.LinkedHashMap;
  */
 public class MemoryMeasureTest {
     public static void main(String[] args) {
-       //Object a = new A();
-        Object a = new LinkedHashMap<Integer,String>();
+        Object a = new A();
         ObjectGraphMeasurer.Footprint fp = ObjectGraphMeasurer.measure(a);
         long size = MemoryMeasurer.measureBytes(a);
         System.err.println(">>> " + fp + "   size: " + size);
         System.err.println("   >>> approx size:" + countSizeApprox(fp, true));
+
+        testMultiDimArray();
     }
+
+    // arrays hold same data space (1,000,000 integers) but take different size in memory in 1.5 times.
+    // cool!!!
+    private static void testMultiDimArray() {
+        Object arr1 = new int[1000][100][10];
+        Object arr2 = new int[10][100][1000];
+        ObjectGraphMeasurer.Footprint fp1 = ObjectGraphMeasurer.measure(arr1);
+        ObjectGraphMeasurer.Footprint fp2 = ObjectGraphMeasurer.measure(arr2);
+        System.err.println("arr2 " + fp1 + "   size: " + MemoryMeasurer.measureBytes(arr1));
+        System.err.println("arr2 " + fp2 + "   size: " + MemoryMeasurer.measureBytes(arr2));
+    }
+
 
     /** My own version.
      * @param footprint
