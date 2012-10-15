8c965a8e4e00b923ace61f905d302343a57f17b8
divannn <unique-id@mail.ru>
2012-10-15 19:52:57 +0400

    MOved to misc repo.

---
+++ src/main/java/com/id/misc/UrlEncoding.java
@@ -0,0 +1,17 @@
+package com.id.misc;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
+public class UrlEncoding {
+    public static void main(String[] args) throws UnsupportedEncodingException, URISyntaxException, MalformedURLException {
+        String s = "http://localhost:9004/dpa-api/reportmenu?query=name=All that*";
+        System.err.println("         str:" + s);
+        System.err.println("html encoded:" + URLEncoder.encode(s, "UTF-8"));
+        URI uri = new URI("http", null, "localhost", 9004, "/dpa-api/reportmenu", "query=name=All that*", null);
+        System.err.println(" url encoded:" + uri.toURL());//or use uri.toASCIIString().
+    }
+}
