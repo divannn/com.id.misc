8d99122880c2714fa6a27fd0dde2b1b4b0b61f85
divannn <divannn@mail.ru>
2017-11-10 18:29:39 +0300

    more  cases

--- jscp/src/main/java/com/id/misc/jscp/ArrayType.java
+++ jscp/src/main/java/com/id/misc/jscp/ArrayType.java
@@ -8,8 +8,11 @@ public class ArrayType {
         System.err.println(">>> " + (arr1d instanceof Object));//true
 
         int[][] arr2d = {{}};
-        //System.err.println(">>> " + (arr2d instanceof int[]));//won't compile.
+        //System.err.println(">>> " + (arr2d instanceof int[]));//error.
+        //System.err.println(">>> " + (arr2d instanceof Object[][]));//error
         System.err.println(">>> " + (arr2d instanceof int[][]));//true
         System.err.println(">>> " + (arr2d instanceof Object[]));//true
+        System.err.println(">>> " + (arr2d instanceof Object));//true
+
     }
 }
